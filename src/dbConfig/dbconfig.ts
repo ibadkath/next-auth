d517799410aafe91bd81190206c22460318419aa
ibadkath <ibadkath88@gmail.com>
2025-01-17 23:26:42 +0500

    resolved

--- src/dbConfig/dbconfig.ts
+++ src/dbConfig/dbconfig.ts
@@ -1,22 +1,18 @@
 import mongoose from "mongoose";
 
-export async function connect(){
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
 
-    try {
-        mongoose.connect(process.env.MONGO_URL!)
-
-        const connection= mongoose.connection;
-        connection.on("connected", ()=>{
-            console.log("MongoDB is connected");
-            
-        })
-        connection.on("error", (err)=>{
-             console.log("MongoDB connection error:", err);
-             
-        })
-
-    } catch (error) {
-        console.log("Something went wrong in connecting db")
-        console.log(error)
-    }
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("MongoDB is connected");
+    });
+    connection.on("error", (err) => {
+      console.log("MongoDB connection error:", err);
+    });
+  } catch (error) {
+    console.log("Something went wrong in connecting db");
+    console.log(error);
+  }
 }
